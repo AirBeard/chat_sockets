c68c99eaeed2cb8f726c44e4856794a28a46602f
AirBeard <prosttak5@gmail.com>
2020-11-05 00:03:34 +0200

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Client
+{
+    
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ник: ");
+            string nickname = Console.ReadLine();
+            socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6000));
+            socket.Send(Encoding.UTF8.GetBytes(nickname));
+            Task.Run(Receive);
+            while(true)
+            {
+                string message = Console.ReadLine();
+                if (message != "")
+                    socket.Send(Encoding.UTF8.GetBytes(message));
+            }
+        }
+        static void Receive()
+        {
+            byte[] buffer = new byte[4096];
+            int count;
+            try
+            {
+                while (true)
+                {
+                    count = socket.Receive(buffer);
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, count));
+                }
+            }
+            catch (SocketException)
+            {
+                socket.Close();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static TcpListener listener= new TcpListener(IPAddress.Parse("127.0.0.1"),6000);
+        static Dictionary<Socket, String> users = new Dictionary<Socket, string>();
+        static void Main(string[] args)
+        {
+            listener.Start();
+            Console.WriteLine("Сервер запущен!");
+            byte[] buffer = new byte[4096];
+            int count;
+            while (true)
+            {
+                Socket s = listener.AcceptSocket();
+                count = s.Receive(buffer);
+                string nickname = Encoding.UTF8.GetString(buffer, 0, count);
+                foreach (Socket u in users.Keys)
+                    u.Send(Encoding.UTF8.GetBytes(nickname +" подключился к чату"));
+                users.Add(s,nickname);
+                Console.WriteLine(nickname + " подключился к чату");
+                Task.Run(() => Receive(s));
+            }
+        }
+        static void Receive(Socket s) 
+        {
+            byte[] buffer = new byte[4096];
+            int count;
+            try
+            {
+                while (true)
+                {
+                    count = s.Receive(buffer);
+                    string message = users[s] + ">> " + Encoding.UTF8.GetString(buffer, 0, count);
+                    foreach (Socket u in users.Keys)
+                        u.Send(Encoding.UTF8.GetBytes(message));
+                }
+            }
+            catch (SocketException)
+            {
+                users.Remove(s);
+            }
+        }
+    }
+}
